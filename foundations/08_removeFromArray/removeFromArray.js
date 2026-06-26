3aea51710d1fc23d305daa406457a5695cf7fb1a
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-06-26 18:36:26 +0900

    Create function removeFromArray()

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...excluded) {
+    for (excludedNum of excluded) {
+        while (array.indexOf(excludedNum) > -1) {
+            array.splice(array.indexOf(excludedNum), 1);
+
+            if (array.indexOf(excludedNum) == -1) {
+                break;
+            }
+        }
+    }
+    
+    return array;
 };
 
 // Do not edit below this line
