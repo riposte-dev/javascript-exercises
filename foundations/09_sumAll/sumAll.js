427cfc54796cb5fff6c51d1add8c34b0c1e5d650
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-06-26 18:47:56 +0900

    Create function sumAll()

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    }
 
+    let sum = 0;
+    let upperBound = a < b ? b : a;
+    let lowerBound = a < b ? a : b;
+
+    for (let i = lowerBound; i <= upperBound; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
