9a752d34f81934de57fa75b845f5dc3c06ba8d03
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-06-26 19:01:13 +0900

    Create functions convertToCelsius() and convertToFahrenheit()

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsiusPrecise = (5 / 9) * (fahrenheit - 32);
+  let celsiusRounded = Math.round(celsiusPrecise * 10) / 10;
+  return celsiusRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheitPrecise = (9 / 5) * celsius + 32;
+  let fahrenheitRounded = Math.round(fahrenheitPrecise * 10) / 10;
+  return fahrenheitRounded;
 };
 
 // Do not edit below this line
