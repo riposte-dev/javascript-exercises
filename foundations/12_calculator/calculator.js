e0fbc073d9ba6c31855cf22174b5b3c16171e90d
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-07-11 17:11:38 +0900

    Calculate one number exponentiated by another

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -14,8 +14,8 @@ const multiply = function(array) {
   return array.reduce((product, current) => product * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
 const factorial = function() {
