7fd50e1567b7584f368fd88d086b6d609014cd51
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-07-11 22:01:33 +0900

    Return the n-th fibonacci number

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else if (n == 2) {
+        return 1;
+    }
 
+    return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
 // Do not edit below this line
