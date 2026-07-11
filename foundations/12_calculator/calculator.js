5a6481f15223432d377b807c486c659f54b960f1
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-07-11 17:09:43 +0900

    Calculate the product of numbers in a given array

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -10,8 +10,8 @@ const sum = function(array) {
   return array.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current, 1);
 };
 
 const power = function() {
