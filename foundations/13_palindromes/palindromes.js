b391ad14dfd618f174c3e9326e0072a44f9dc679
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-07-11 21:54:08 +0900

    Change forwardWord and backwardWord case to lower during their declarations

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -3,10 +3,10 @@ const punctuationList = [" ", ",", ".", "!", "?"];
 const palindromes = function(string) {
     const letters = string.split("").filter(letter => !punctuationList.includes(letter));
 
-    let forwardWord = letters.toString();
-    let backwardWord = letters.toReversed().toString();
+    let forwardWord = letters.toString().toLowerCase();
+    let backwardWord = letters.toReversed().toString().toLowerCase();
 
-    if (forwardWord.toLowerCase() == backwardWord.toLowerCase()) {
+    if (forwardWord == backwardWord) {
         return true;
     } else {
         return false;
