bce4440a58805f288ee1a6407070a4348372ced9
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-07-11 22:21:47 +0900

    Return the oldest person after sorting by age

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.sort((a, b) => (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth));
 
+    return people[0];
 };
 
 // Do not edit below this line
